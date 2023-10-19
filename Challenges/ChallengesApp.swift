eb40f1a654f00ba1e38b3711b52eb1f1e04806f6
Daijiro Tsushima <daijiro.tsushima@gmail.com>
2023-10-19 13:12:41 -0400

    Initial Commit

---
+++ Challenges/ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengesApp.swift
+//  Challenges
+//
+//  Created by Daijiro Tsushima on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenges/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenges
+//
+//  Created by Daijiro Tsushima on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
